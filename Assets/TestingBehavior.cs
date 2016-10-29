8a5264d10c994b036eb3bf45985b74a4eaa973c5
mat9054 <mat_9055@hotmail.com>
2016-10-29 19:27:59 -0400

    First Check In
    git project

---
+++ Assets/TestingBehavior.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestingBehavior : MonoBehaviour {
+
+    public int counter = 10;
+    public float horizontalSpacing = 2f;
+    public float verticalSpacing = 1f;
+    public float timer = 3;
+    public float nextTime = 0f;
+    public int minHeight = 1;
+    public int maxHeight = 10;
+
+
+    // Use this for initialization
+    void Start () {
+    
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (counter > 0 && Time.fixedTime > nextTime)
+        {
+            nextTime = Time.fixedTime + timer;
+            for (int z = 0; z < 10; z++)
+            {
+                int random = Random.Range(minHeight, maxHeight);
+                for (int i = 0; i < random; i++)
+                {
+                    CustomerBox cbox = new CustomerBox();
+                    cbox.box.transform.position = new Vector3(counter * horizontalSpacing, i * verticalSpacing, z * horizontalSpacing);
+                    cbox.PickRandomColor();
+                }
+            }
+            counter--;
+        }
+        
+
+    }
+
+    class CustomerBox
+    {
+        public GameObject box = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+        public void PickRandomColor()
+        {
+            box.GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value, Random.value);
+        }
+    }
+}
